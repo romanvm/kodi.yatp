2e368f07fc65fc30f3c27b54f73863879a4f8f0c
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2015-07-17 13:36:35 +0300

    Moved json-rpc requests into a separate module

---
+++ plugin.video.yatp/libs/json_requests.py
@@ -0,0 +1,55 @@
+# coding: utf-8
+# Module: json_requests
+# Created on: 17.07.2015
+# Author: Roman Miroshnychenko aka Roman V.M. (romanvm@yandex.ua)
+
+from requests import post
+from addon import Addon
+
+json_rpc_url = Addon().torrenter_host + '/json-rpc'
+
+
+def _request(data):
+    """
+    Send JSON-RPC request
+
+    :param data:
+    :return:
+    """
+    reply = post(json_rpc_url, json=data).json()
+    try:
+        return reply['result']
+    except KeyError:
+        raise RuntimeError(reply['error'])
+
+
+def add_torrent(torrent):
+    _request({'method': 'add_torrent', 'params': [torrent]})
+
+
+def check_torrent_added():
+    return _request({'method': 'check_torrent_added'})
+
+
+def get_data_buffer():
+    return _request({'method': 'get_data_buffer'})
+
+
+def stream_torrent(info_hash, file_index, buffer_size):
+    _request({'method': 'stream_torrent', 'params': [info_hash, file_index, buffer_size]})
+
+
+def check_buffering_complete():
+    return _request({'method': 'check_buffering_complete'})
+
+
+def get_torrent_info(info_hash):
+    return _request({'method': 'get_torrent_info', 'params': [info_hash]})
+
+
+def abort_buffering():
+    _request({'method': 'abort_buffering'})
+
+
+def remove_torrent(info_hash, delete_files):
+    _request({'method': 'remove_torrent', 'params': [info_hash, delete_files]})
--- plugin.video.yatp/libs/streamer.py
+++ plugin.video.yatp/libs/streamer.py
@@ -5,30 +5,14 @@
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 
 import os
-from urlparse import urljoin
 from time import sleep
-from requests import post
 import xbmcgui
+import json_requests as jsonrc
 from addon import Addon
 
 
 __addon__ = Addon()
-json_url = __addon__.torrenter_host + '/json-rpc'
 media_url = __addon__.torrenter_host + '/media/'
-
-
-def _request(data):
-    """
-    Send JSON-RPC request
-
-    :param data:
-    :return:
-    """
-    reply = post(json_url, json=data).json()
-    try:
-        return reply['result']
-    except KeyError:
-        raise RuntimeError(reply['error'])
 
 
 def get_path(torrent):
@@ -38,13 +22,13 @@ def get_path(torrent):
     :param torrent: str - magnet link or .torrent file URL
     :return:
     """
-    _request({'method': 'add_torrent', 'params': [torrent]})
-    progress = xbmcgui.DialogProgress()
-    progress.create('Buffering torrent', 'Adding torrent...', 'This may take some time.')
-    while not (progress.iscanceled() or _request({'method': 'check_torrent_added', 'params': None})):
+    jsonrc.add_torrent(torrent)
+    progress_dialog = xbmcgui.DialogProgress()
+    progress_dialog.create('Buffering torrent', 'Adding torrent...', 'This may take some time.')
+    while not progress_dialog.iscanceled() or jsonrc.check_torrent_added():
         sleep(1.0)
-    if not progress.iscanceled():
-        torrent_data = _request({'method': 'get_data_buffer', 'params': None})
+    if not progress_dialog.iscanceled():
+        torrent_data = jsonrc.get_data_buffer()
         __addon__.log(str(torrent_data))
         # Create a list of videofiles in a torrent.
         # Each element is a tuple (<file name>, <file index in a torrent>).
@@ -59,35 +43,29 @@ def get_path(torrent):
                 index = 0
             if index >= 0:
                 # Select a vileofile to play
-                videofile = videofiles[index]
-                _request({'method': 'stream_torrent', 'params': [torrent_data['info_hash'],
-                                                                 videofile[0],
-                                                                 __addon__.buffer_size]})
-                while not (progress.iscanceled() or
-                               _request({'method': 'check_buffering_complete', 'params': None})):
-                    buffer_progress = _request({'method': 'get_data_buffer', 'params': None})
-                    torrent_info = _request({'method': 'get_torrent_info',
-                                             'params': [torrent_data['info_hash']]})
-                    progress.update(buffer_progress,
+                selected_file_index = videofiles[index][0]
+                jsonrc.stream_torrent(torrent_data['info_hash'], selected_file_index,  __addon__.buffer_size)
+                while not progress_dialog.iscanceled() or jsonrc.check_buffering_complete():
+                    buffer_progress = jsonrc.get_data_buffer()
+                    torrent_info = jsonrc.get_torrent_info(torrent_data['info_hash'])
+                    progress_dialog.update(buffer_progress,
                                     'Downloaded: {0}MB'.format(torrent_info['total_download']),
                                     'Download speed: {0}KB/s'.format(torrent_info['dl_speed']),
                                     'Seeds: {0}'.format(torrent_info['num_seeds']))
                     sleep(1.0)
-                progress.close()
-                if not progress.iscanceled():
-                    return media_url + torrent_data['files'][videofile[0]].replace('\\', '/')
+                progress_dialog.close()
+                if not progress_dialog.iscanceled():
+                    return media_url + torrent_data['files'][selected_file_index].replace('\\', '/')
                 else:
-                    _request({'method': 'abort_buffering', 'params': None})
-                    if (_request({'method': 'get_torrent_info', 'params': [torrent_data['info_hash']]})['progress'] <=
-                            __addon__.buffer_size):
+                    jsonrc.abort_buffering()
+                    if (jsonrc.get_torrent_info(torrent_data['info_hash'])['progress'] <= __addon__.buffer_size):
                         sleep(0.5)
-                        _request({'method': 'remove_torrent', 'params': [torrent_data['info_hash'], True]})
+                        jsonrc.remove_torrent(torrent_data['info_hash'], True)
             else:
                 xbmcgui.Dialog().notification(__addon__.id, 'A video is not selected', __addon__.icon, 3000)
         else:
             xbmcgui.Dialog().notification(__addon__.id, 'No videofiles to play.', 'error', 3000)
-    if not (_request({'method': 'check_torrent_added', 'params': None})
-            and _request({'method': 'check_buffering_complete', 'params': None})):
+    if not (jsonrc.check_torrent_added() and jsonrc.check_buffering_complete()):
         xbmcgui.Dialog().notification(__addon__.id, 'Playback cancelled.', __addon__.icon, 3000)
-    progress.close()
+    progress_dialog.close()
     return None
