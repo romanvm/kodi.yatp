aad9f62f3ad45a9dd79e48bc48115ee913cf6502
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2015-07-23 12:27:44 +0300

    Moved Bottle wsgi application into a separate module

---
+++ plugin.video.yatp/libs/wsgi.py
@@ -0,0 +1,166 @@
+# coding: utf-8
+# Module: wsgi
+# Created on: 23.07.2015
+# Author: Roman Miroshnychenko aka Roman V.M. (romanvm@yandex.ua)
+"""
+Torrent streamer WSGI application for Web/JSON interface
+"""
+
+DEBUG = True
+
+import os
+import sys
+from cStringIO import StringIO
+from json import dumps
+from inspect import getmembers, isfunction
+from bottle import route, default_app, request, template, response, debug, static_file, TEMPLATE_PATH, HTTPError
+import methods
+from addon import Addon
+from torrenter import Torrenter, libtorrent
+from timers import Timer, check_seeding_limits, save_resume_data
+
+__addon__ = Addon()
+# Torrent client parameters
+download_dir = __addon__.download_dir
+resume_dir = os.path.join(__addon__.config_dir, 'torrents')
+if not os.path.exists(resume_dir):
+    os.mkdir(resume_dir)
+torrent_port = __addon__.torrent_port
+torrenter = Torrenter(torrent_port, torrent_port + 10, True, resume_dir)
+# Timers
+max_ratio = __addon__.ratio_limit
+max_time = __addon__.time_limit
+limits_timer = Timer(10, check_seeding_limits, torrenter, max_ratio, max_time,
+                     __addon__.expired_action, __addon__.delete_expired_files)
+save_resume_timer = Timer(20, save_resume_data, torrenter)
+# Bottle WSGI application
+static_path = os.path.join(__addon__.path, 'resources', 'web')
+TEMPLATE_PATH.insert(0, os.path.join(static_path, 'templates'))
+debug(DEBUG)
+
+
+@route('/')
+def root():
+    """
+    Root path
+
+    :return:
+    """
+    user, password = request.auth or (None, None)
+    if __addon__.pass_protect and (user is None or (user, password) != __addon__.credentials):
+        error = HTTPError(401, 'Access denied')
+        error.add_header('WWW-Authenticate', 'Basic realm="private"')
+        return error
+    else:
+        return template('torrents')
+
+
+@route('/json-rpc', method='GET')
+def get_methods():
+    """
+    Display the list of available methods
+
+    :return:
+    """
+    methods_list = []
+    docs_list = []
+    for member in getmembers(methods):
+        if isfunction(member[1]) and not member[0].startswith('_'):
+            methods_list.append(member[0])
+            docs_list.append(member[1].__doc__.replace('\n', '<br>'))
+    return template('methods', methods=methods_list, docs=docs_list)
+
+
+@route('/json-rpc', method='POST')
+def json():
+    """
+    JSON-RPC requests processing
+
+    :return:
+    """
+    if DEBUG:
+        __addon__.log('***** JSON request *****')
+        __addon__.log(request.body.read())
+    data = request.json
+    # Use the default download dir if param[2] == ''
+    if data['method'] == 'add_torrent' and len(data['params']) >= 2 and not data['params'][1]:
+        data['params'][1] = download_dir
+    # Use the default download dir if param[2] is missing
+    elif data['method'] == 'add_torrent' and len(data['params']) == 1:
+        data['params'].append(download_dir)
+    if data['method'] == 'add_torrent' and len(data['params']) == 2:
+        data['params'].append(True)
+    reply = {'jsonrpc': '2.0', 'id': data.get('id', '1')}
+    try:
+        reply['result'] = getattr(methods, data['method'])(torrenter, data.get('params'))
+    except Exception, ex:
+        reply['error'] = '{0}: {1}'.format(str(ex.__class__)[7:-2], ex.message)
+    if DEBUG:
+        __addon__.log('***** JSON response *****')
+        __addon__.log(str(reply))
+    return reply
+
+
+@route('/torrents-json')
+def get_torrents():
+    """
+    Get the list of available torrents with their params wrapped in JSON
+
+    :return:
+    """
+    response.content_type = 'application/json'
+    return dumps(torrenter.get_all_torrents_info())
+
+
+@route('/media/<path:path>')
+def get_media(path):
+    """
+    Serve media files
+
+    :param path: relative path to a media file
+    :return:
+    """
+    __addon__.log('Playing media: ' + path.decode('utf-8'))
+    if sys.platform == 'win32':
+        path = path.decode('utf-8')
+    if os.path.splitext(path)[1] == '.mkv':
+        mime = 'video/x-matroska'
+    elif os.path.splitext(path)[1] == '.mp4':
+        mime = 'video/mp4'
+    elif os.path.splitext(path)[1] == '.avi':
+        mime = 'video/avi'
+    elif os.path.splitext(path)[1] == '.ts':
+        mime = 'video/mp2t'
+    elif os.path.splitext(path)[1] == '.mov':
+        mime = 'video/quicktime'
+    else:
+        mime = 'auto'
+    return static_file(path, root=download_dir, mimetype=mime)
+
+
+@route('/static/<path:path>')
+def get_static(path):
+    """
+    Serve static files
+
+    :param path: relative path to a static file
+    :return:
+    """
+    return static_file(path, root=static_path)
+
+
+@route('/add-torrent-file', method='POST')
+def add_torrent_file():
+    """
+    Add .torrent file
+
+    :return:
+    """
+    buffer = StringIO()
+    upload = request.files.get('torrent_file')
+    upload.save(buffer)
+    torrent = libtorrent.bdecode(buffer.getvalue())
+    torrenter.add_torrent(torrent, download_dir)
+
+
+application = default_app()
--- plugin.video.yatp/server.py
+++ plugin.video.yatp/server.py
@@ -4,192 +4,34 @@
 # Author: Roman Miroshnychenko aka Roman V.M. (romanvm@yandex.ua)
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 """
-Torrenter WSGI application and server
+Torrent streamer WSGI server
 """
 
 from libs.addon import Addon
-
 __addon__ = Addon()
-if __addon__.remote_mode:
-    sys.exit()
-__addon__.log('***** Torrent Server starting... *******')
+__addon__.log('***** Starting Torrent Server... *****')
 
-import sys
-import os
-from cStringIO import StringIO
-from inspect import getmembers, isfunction
-from json import dumps
 from time import sleep
 import xbmc
 import xbmcgui
-from libs import methods
-from libs.bottle import Bottle, request, template, response, debug, static_file, TEMPLATE_PATH, HTTPError
+from libs import wsgi
 from libs.wsgi_server import create_server
-from libs.torrenter import Torrenter, libtorrent
-from libs.timers import Timer, check_seeding_limits, save_resume_data
 
-DEBUG = True
-
-static_path = os.path.join(__addon__.path, 'resources', 'web')
-TEMPLATE_PATH.insert(0, os.path.join(static_path, 'templates'))
-debug(DEBUG)
-# Create Bottle WSGI application
-app = Bottle()
-# Torrent server parameters
-torrent_dir = __addon__.download_dir
-resume_dir = os.path.join(__addon__.config_dir, 'torrents')
-if not os.path.exists(resume_dir):
-    os.mkdir(resume_dir)
-max_ratio = __addon__.ratio_limit
-max_time = __addon__.time_limit
-torrent_port = __addon__.torrent_port
 server_port = __addon__.server_port
-#-------------------------------------
-torrenter = Torrenter(torrent_port, torrent_port + 10, True, resume_dir)
-limits_timer = Timer(10, check_seeding_limits, torrenter, max_ratio, max_time,
-                     __addon__.expired_action, __addon__.delete_expired_files)
-save_resume_timer = Timer(20, save_resume_data, torrenter)
-
-
-@app.route('/')
-def root():
-    """
-    Root path
-
-    :return:
-    """
-    user, password = request.auth or (None, None)
-    if __addon__.pass_protect and (user is None or (user, password) != __addon__.credentials):
-        error = HTTPError(401, 'Access denied')
-        error.add_header('WWW-Authenticate', 'Basic realm="private"')
-        return error
-    else:
-        return template('torrents')
-
-
-@app.route('/json-rpc', method='GET')
-def get_methods():
-    """
-    Display the list of available methods
-
-    :return:
-    """
-    methods_list = []
-    docs_list = []
-    for member in getmembers(methods):
-        if isfunction(member[1]) and not member[0].startswith('_'):
-            methods_list.append(member[0])
-            docs_list.append(member[1].__doc__.replace('\n', '<br>'))
-    return template('methods', methods=methods_list, docs=docs_list)
-
-
-@app.route('/json-rpc', method='POST')
-def json():
-    """
-    JSON-RPC requests processing
-
-    :return:
-    """
-    if DEBUG:
-        __addon__.log('***** JSON request *****')
-        __addon__.log(request.body.read())
-    data = request.json
-    # Use the default download dir if param[2] == ''
-    if data['method'] == 'add_torrent' and len(data['params']) >= 2 and not data['params'][1]:
-        data['params'][1] = torrent_dir
-    # Use the default download dir if param[2] is missing
-    elif data['method'] == 'add_torrent' and len(data['params']) == 1:
-        data['params'].append(torrent_dir)
-    if data['method'] == 'add_torrent' and len(data['params']) == 2:
-        data['params'].append(True)
-    reply = {'jsonrpc': '2.0', 'id': data.get('id', '1')}
-    try:
-        reply['result'] = getattr(methods, data['method'])(torrenter, data.get('params'))
-    except Exception, ex:
-        reply['error'] = '{0}: {1}'.format(str(ex.__class__)[7:-2], ex.message)
-    if DEBUG:
-        __addon__.log('***** JSON response *****')
-        __addon__.log(str(reply))
-    return reply
-
-
-@app.route('/torrents-json')
-def get_torrents():
-    """
-    Get the list of available torrents with their params wrapped in JSON
-
-    :return:
-    """
-    response.content_type = 'application/json'
-    return dumps(torrenter.get_all_torrents_info())
-
-
-@app.route('/media/<path:path>')
-def get_media(path):
-    """
-    Serve media files
-
-    :param path: relative path to a media file
-    :return:
-    """
-    __addon__.log('Playing media: ' + path.decode('utf-8'))
-    if sys.platform == 'win32':
-        path = path.decode('utf-8')
-    if os.path.splitext(path)[1] == '.mkv':
-        mime = 'video/x-matroska'
-    elif os.path.splitext(path)[1] == '.mp4':
-        mime = 'video/mp4'
-    elif os.path.splitext(path)[1] == '.avi':
-        mime = 'video/avi'
-    elif os.path.splitext(path)[1] == '.ts':
-        mime = 'video/mp2t'
-    elif os.path.splitext(path)[1] == '.mov':
-        mime = 'video/quicktime'
-    else:
-        mime = 'auto'
-    return static_file(path, root=torrent_dir, mimetype=mime)
-
-
-@app.route('/static/<path:path>')
-def get_static(path):
-    """
-    Serve static files
-
-    :param path: relative path to a static file
-    :return:
-    """
-    return static_file(path, root=static_path)
-
-
-@app.route('/add-torrent-file', method='POST')
-def add_torrent_file():
-    """
-    Add .torrent file
-
-    :return:
-    """
-    buffer = StringIO()
-    upload = request.files.get('torrent_file')
-    upload.save(buffer)
-    torrent = libtorrent.bdecode(buffer.getvalue())
-    torrenter.add_torrent(torrent, torrent_dir)
-
-
-if __name__ == '__main__':
-    sleep(3.0)
-    start_trigger = True
-    httpd = create_server(app, port=server_port)
-    httpd.timeout = 0.1
-    limits_timer.start()
-    save_resume_timer.start()
-    while not xbmc.abortRequested:
-        httpd.handle_request()
-        if start_trigger:
-            xbmcgui.Dialog().notification('YATP', 'Torrent server started', __addon__.icon, 3000, False)
-            __addon__.log('***** Torrent Server started *******')
-            start_trigger = False
-    limits_timer.abort()
-    save_resume_timer.abort()
-    torrenter.abort_buffering()
-    del torrenter
-    __addon__.log('***** Torrent Server stopped *******')
+sleep(3.0)
+start_trigger = True
+httpd = create_server(wsgi.application, port=server_port)
+httpd.timeout = 0.1
+wsgi.limits_timer.start()
+wsgi.save_resume_timer.start()
+while not xbmc.abortRequested:
+    httpd.handle_request()
+    if start_trigger:
+        xbmcgui.Dialog().notification('YATP', 'Torrent server started', __addon__.icon, 3000, False)
+        __addon__.log('***** Torrent Server started *******')
+        start_trigger = False
+wsgi.limits_timer.abort()
+wsgi.save_resume_timer.abort()
+wsgi.torrenter.abort_buffering()
+del wsgi.torrenter
+__addon__.log('***** Torrent Server stopped *******')
