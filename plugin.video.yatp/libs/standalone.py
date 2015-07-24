fd9afb66d0e33b859bd13834f6724eb9eaaaa340
Roman Miroshnychenko <romanvm@yandex.ru>
2015-07-24 14:16:40 +0300

    Added the ability to run torrent server in standalone mode (withoun Kodi)

---
+++ plugin.video.yatp/libs/standalone.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+# Module: standalone
+# Created on: 24.07.2015
+# Author: Roman Miroshnychenko aka Roman V.M.
+# E-mail: romanvm@yandex.ua
+
+import os
+import codecs
+import json
+
+DEFAULT_CONFIG = {'config_dir': './resume',
+                  'download_dir': 'D:\\Torrents',  # Change to your dir here
+                  'buffer_size': 35,  # MB
+                  'ratio_limit': 0.0,
+                  'time_limit': 0,
+                  'expired_action': 'pause',  # Possible values 'pause' and 'delete'
+                  'delete_expired_files': False,
+                  'torrent_port': 25333,
+                  'server_port': 8668,
+                  'pass_protect': False,
+                  'login': 'yatp',
+                  'password': 'yatp'}
+
+
+class ConfigParser(object):
+    """
+    Config parser for standalone torrent server
+    """
+    def __init__(self):
+        self._config = None
+        config_file = os.path.join(os.path.dirname(__file__), '..', 'settings.json')
+        if not os.path.exists(config_file):
+            with codecs.open(config_file, 'wb', encoding='utf-8') as file_:
+                json.dump(DEFAULT_CONFIG, file_, ensure_ascii=False, indent=2)
+            self._config = DEFAULT_CONFIG
+        else:
+            with codecs.open(config_file, 'rb', encoding='utf-8') as file_:
+                self._config = json.load(file_, encoding='utf-8')
+        if not os.path.exists(self._config['config_dir']):
+            os.mkdir(self._config['config_dir'])
+
+    def log(self, message):
+        print message
+
+    @property
+    def credentials(self):
+        return (self._config['login'], self._config['password'])
+
+    @property
+    def path(self):
+        return os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+    def __getattr__(self, item):
+        return self._config[item]
--- plugin.video.yatp/server.py
+++ plugin.video.yatp/server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2
 # coding: utf-8
 # Module: server
 # Created on: 01.07.2015
@@ -7,17 +8,23 @@
 Torrent streamer WSGI server
 """
 
-from time import sleep
+import sys
+standalone = '-s' in sys.argv or '--standalone' in sys.argv
 
-sleep(3.0)
+if not standalone:
+    from time import sleep
+    sleep(3.0)
 
 from libs.addon import Addon
 
 __addon__ = Addon()
 __addon__.log('***** Starting Torrent Server... *****')
 
-import xbmc
-import xbmcgui
+try:
+    import xbmc
+    import xbmcgui
+except ImportError:
+    pass
 from libs.server import wsgi
 from libs.server.wsgi_server import create_server
 
@@ -26,12 +33,18 @@ wsgi.save_resume_timer.start()
 httpd = create_server(wsgi.application, port=__addon__.server_port)
 httpd.timeout = 0.1
 start_trigger = True
-while not xbmc.abortRequested:
-    httpd.handle_request()
-    if start_trigger:
-        xbmcgui.Dialog().notification('YATP', 'Torrent server started', __addon__.icon, 3000, False)
-        __addon__.log('***** Torrent Server started *****')
-        start_trigger = False
+try:
+    while standalone or not xbmc.abortRequested:
+            httpd.handle_request()
+            if start_trigger:
+                __addon__.log('***** Torrent Server started *****')
+                if not standalone:
+                    xbmcgui.Dialog().notification('YATP', 'Torrent server started', __addon__.icon, 3000, False)
+                else:
+                    print 'Press CTRL+C to exit'
+                start_trigger = False
+except KeyboardInterrupt:
+    pass
 wsgi.limits_timer.abort()
 wsgi.save_resume_timer.abort()
 wsgi.torrenter.abort_buffering()
