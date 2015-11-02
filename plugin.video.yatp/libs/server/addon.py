a51eba13bbb64c20ced6ea8caa617d526719e4f5
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2015-11-02 13:34:54 +0200

    Fixed encoding issue with download foder name containing non-ascii chars, version bump

--- plugin.video.yatp/libs/server/addon.py
+++ plugin.video.yatp/libs/server/addon.py
@@ -16,7 +16,7 @@ class Addon(simpleplugin.Addon):
     """Helper class to access addon parameters"""
     def __init__(self):
         super(Addon, self).__init__()
-        self._download_dir = (self.get_setting('download_dir') or
+        self._download_dir = (self.get_setting('download_dir').decode('utf-8') or
                               xbmc.translatePath('special://temp').decode('utf-8'))
         if not os.path.exists(self._download_dir):
             os.mkdir(self._download_dir)
