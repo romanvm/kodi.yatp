3d7d35449bdc6412e3cf3168ccb2242f43ba1751
Roman Miroshnychenko <romanvm@yandex.ua>
2016-11-08 16:23:43 +0200

    Refactors commands according to the new simpleplugin API

--- plugin.video.yatp/libs/client/commands.py
+++ plugin.video.yatp/libs/client/commands.py
@@ -14,10 +14,7 @@ import json_requests as jsonrq
 from simpleplugin import Addon
 
 addon = Addon('plugin.video.yatp')
-
-
-def string(id_):
-    return addon.get_localized_string(id_)
+_ = addon.initialize_gettext()
 
 
 def show_torrent_info(info_hash):
@@ -32,9 +29,20 @@ def show_torrent_info(info_hash):
     info_dialog.create(torr_info['name'])
     while not info_dialog.iscanceled():
         info_dialog.update(torr_info['progress'],
-                           string(32011).format(torr_info['state'], torr_info['num_seeds'], torr_info['num_peers']),
-                           string(32012).format(torr_info['size'], torr_info['dl_speed'], torr_info['ul_speed']),
-                           string(32013).format(torr_info['total_download'], torr_info['total_upload']))
+                           _('state: {0}; seeds: {1}; peers: {2}').format(
+                               torr_info['state'],
+                               torr_info['num_seeds'],
+                               torr_info['num_peers']
+                           ),
+                           _('size: {0}MB; DL speed: {1}KB/s; UL speed: {2}KB/s').format(
+                               torr_info['size'],
+                               torr_info['dl_speed'],
+                               torr_info['ul_speed']
+                           ),
+                           _('total DL: {0}MB; total UL: {1}MB').format(
+                               torr_info['total_download'],
+                               torr_info['total_upload'])
+                           )
         xbmc.sleep(1000)
         torr_info = jsonrq.get_torrent_info(info_hash)
 
@@ -44,9 +52,14 @@ if __name__ == '__main__':
         jsonrq.pause_torrent(sys.argv[2])
     elif sys.argv[1] == 'resume':
         jsonrq.resume_torrent(sys.argv[2])
-    elif sys.argv[1] == 'delete' and xbmcgui.Dialog().yesno(string(32024), string(32025)):
+    elif sys.argv[1] == 'delete' and xbmcgui.Dialog().yesno(
+            _('Confirm delete'),
+            _('Do you really want to delete the torrent?')):
         jsonrq.remove_torrent(sys.argv[2], False)
-    elif sys.argv[1] == 'delete_with_files'and xbmcgui.Dialog().yesno(string(32024), string(32026), string(32027)):
+    elif sys.argv[1] == 'delete_with_files'and xbmcgui.Dialog().yesno(
+            _('Confirm delete'),
+            _('Do you really want to delete the torrent with files?'),
+            _('Warning: The files will be deleted permanently!')):
         jsonrq.remove_torrent(sys.argv[2], True)
     elif sys.argv[1] == 'pause_all':
         jsonrq.pause_all()
@@ -57,5 +70,5 @@ if __name__ == '__main__':
     elif sys.argv[1] == 'restore_finished':
         jsonrq.restore_finished(sys.argv[2])
     else:
-        addon.log('Command cancelled or invalid command: {0}'.format(sys.argv[1]))
+        addon.log_debug('Command cancelled or invalid command: {0}'.format(sys.argv[1]))
     xbmc.executebuiltin('Container.Refresh')
